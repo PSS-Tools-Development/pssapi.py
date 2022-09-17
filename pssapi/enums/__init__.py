163d806d67d5d32a8a62a63c9f119bf383c32f77
solevis <solevis@protonmail.com>
2022-09-17 14:10:48 +0200

    nit

--- pssapi/enums/__init__.py
+++ pssapi/enums/__init__.py
@@ -1,3 +1,7 @@
+"""
+    This file will be generated automatically
+"""
+
 from .character_part_type import CharacterPartType
 from .device_type import DeviceType
 from .enhancement_type import EnhancementType
@@ -6,8 +10,3 @@ from .item_sub_type import ItemSubType
 from .item_type import ItemType
 from .language_key import LanguageKey
 from .module_type import ModuleType
-
-
-##################################################
-##   This file will be generated automatically  ##
-##################################################
