ab01d3ee65e3bf5997cb11ae372ab8b99ea94046
solevis <solevis@protonmail.com>
2022-09-22 21:49:20 +0200

    Add ShipService example in the showcase

--- showcase.py
+++ showcase.py
@@ -1,11 +1,16 @@
 import asyncio as _asyncio
 
 import pssapi
+from pssapi.entities import ShipDesign
 
 
 async def main() -> None:
     client = pssapi.PssApiClient(production_server='api2.pixelstarships.com')
     print(f'Production server: {client.production_server}')
+
+    ship_designs: list[ShipDesign] = await client.ship_service.list_all_ship_designs_2()
+    print(f'Found {len(ship_designs)} ship designs.')
+    print(f'First ship: {ship_designs[0].ship_design_name}')
 
     item_designs = await client.item_service.list_item_designs_2()
     print(f'Found {len(item_designs)} item designs.')
