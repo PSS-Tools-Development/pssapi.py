3de506842b4c43606ed78813886737a4950dbc9e
Zukunftsmusik <5472896+Zukunftsmusik@users.noreply.github.com>
2022-11-08 20:18:40 +0100

    Added functionality to make POST requests

--- pssapi/core.py
+++ pssapi/core.py
@@ -1,3 +1,6 @@
+import json as _json
+from typing import Any as _Any
+from typing import Dict as _Dict
 from typing import List as _List
 from typing import Type as _Type
 from xml.etree import ElementTree as _ElementTree
@@ -7,8 +10,32 @@ import aiohttp as _aiohttp
 from pssapi.entities import EntityBase as _EntityBase
 
 
-async def get_entities_from_path(entity_type: _Type[_EntityBase], xml_parent_tag_name: str, production_server: str, path: str, **params) -> _List[_EntityBase]:
-    raw_xml = await __get_data_from_path(production_server, path, **params)
+__SESSION: _aiohttp.ClientSession = _aiohttp.ClientSession()
+
+
+def create_request_content(structure: str, params: _Dict[str, _Any], content_type: str) -> str:
+    if content_type == 'json':
+        return __create_json_request_content(structure, params)
+    elif content_type == 'xml':
+        pass
+
+
+def __create_json_request_content(structure: str, params: _Dict[str, _Any]) -> str:
+    d = _json.loads(structure)
+    __update_nested_dict_values(d, params)
+    return _json.dumps(d)
+
+
+def __update_nested_dict_values(d: dict, params: _Dict[str, _Any]) -> None:
+    for key, value in d.items():
+        if isinstance(value, dict):
+            __update_nested_dict_values(value, params)
+        else:
+            d[key] = params[key]
+
+
+async def get_entities_from_path(entity_type: _Type[_EntityBase], xml_parent_tag_name: str, production_server: str, path: str, method: str, request_content: str = None, **params) -> _List[_EntityBase]:
+    raw_xml = await __get_data_from_path(production_server, path, method, content=request_content, **params)
     root = _ElementTree.fromstring(raw_xml)
     parent_node = root.find(f'.//{xml_parent_tag_name}')
     result = []
@@ -18,20 +45,24 @@ async def get_entities_from_path(entity_type: _Type[_EntityBase], xml_parent_tag
     return result
 
 
-async def __get_data_from_path(production_server: str, path: str, **params) -> str:
+async def __get_data_from_path(production_server: str, path: str, method: str, content: str = None, **params) -> str:
     if path:
         path = path.strip('/')
     url = f'https://{production_server}/{path}'
-    return await __get_data_from_url(url, **params)
+    return await __get_data_from_url(url, method, content, **params)
 
 
-async def __get_data_from_url(url: str, **params) -> str:
+async def __get_data_from_url(url: str, method: str, content: str = None, **params) -> str:
     # filter parameters with a None value
     params = {key: value for (key, value) in params.items() if value}
 
-    async with _aiohttp.ClientSession() as session:
-        async with session.get(url, params=params) as response:
-            data = await response.text(encoding='utf-8')
+    async with __SESSION as session:
+        if method == 'GET':
+            async with session.get(url, params=params) as response:
+                data = await response.text(encoding='utf-8')
+        elif method == 'POST':
+            async with session.get(url, data=content.encode('utf-8'), params=params) as response:
+                data = await response.text(encoding='utf-8')
     return data
 
 
