159d47069c89dc92ab6b69d598e75475c262bb59
Solevis <solevis@protonmail.com>
2023-03-26 14:13:20 +0200

    Fix test_pss_enum unit test

--- tests/utils/test_parse.py
+++ tests/utils/test_parse.py
@@ -36,13 +36,13 @@ def test_pss_datetime():
 
 def test_pss_enum():
     assert _parse.pss_enum(None, _enums.AchievementType) is None
-    assert _parse.pss_enum("DeviceTypeAndroid", _enums.DeviceType) == _enums.DeviceType.DEVICE_TYPE_ANDROID
-    assert _parse.pss_enum(3, _enums.VisibilityFlags) == _enums.VisibilityFlags.ALWAYSSHOW
+    assert _parse.pss_enum("DeviceTypeAndroid", _enums.DeviceType) == _enums.DeviceType.ANDROID
+    assert _parse.pss_int(3, _enums.VisibilityFlags) == _enums.VisibilityFlags.ALWAYS_SHOW
 
     with _pytest.raises(TypeError):  # The enum doesn't have such a value
         _parse.pss_enum("DeviceType", _enums.DeviceType)
     with _pytest.raises(TypeError):  # The enum doesn't have such a value
-        assert _parse.pss_enum("3", _enums.VisibilityFlags) == _enums.VisibilityFlags.ALWAYSSHOW
+        assert _parse.pss_enum("3", _enums.VisibilityFlags) == _enums.VisibilityFlags.ALWAYS_SHOW
 
 
 def test_pss_float():
