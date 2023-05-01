e5b9a906abe766d6ee53296cc01badc56be47b0d
The worst <5472896+Zukunftsmusik@users.noreply.github.com>
2023-05-01 15:07:43 +0200

    Fixed wrong enum type

--- src/pssapi/enums/visibility_flags.py
+++ src/pssapi/enums/visibility_flags.py
@@ -1,4 +1,4 @@
-from enum import IntFlag as _IntFlag
+from enum import IntEnum as _IntEnum
 
 """
 This file has been be generated from decompilation and might require manual
@@ -6,7 +6,7 @@ fixing, if Savy uses enum values that are python keywords.
 """
 
 
-class VisibilityFlags(_IntFlag):
+class VisibilityFlags(_IntEnum):
     NONE = 0
     HIDE_WHEN_NOT_SATISFIED = 1
     SHOW_WHEN_NOT_SATISFIED = 2
