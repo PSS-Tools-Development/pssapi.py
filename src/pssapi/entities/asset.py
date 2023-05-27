b67e9a6854a658accd9fa64a2029bedecda811b5
The worst <5472896+Zukunftsmusik@users.noreply.github.com>
2023-05-27 14:43:15 +0200

    Added enum properties to Asset

--- src/pssapi/entities/asset.py
+++ src/pssapi/entities/asset.py
@@ -1,12 +1,24 @@
 from ..types import EntityInfo as _EntityInfo
 from .entity_base import EntityWithIdBase as _EntityWithIdBase
 from .raw import AssetRaw as _AssetRaw
+from .. import enums as _enums
+from ..utils import parse as _parse
 
 
 class Asset(_AssetRaw, _EntityWithIdBase):
     def __init__(self, asset_info: _EntityInfo) -> None:
         super().__init__(asset_info)
+        self._asset_type_enum: _enums.AssetType = _parse.pss_str_enum(self.asset_type, _enums.AssetType)
+        self._download_type_enum: _enums.DownloadType = _parse.pss_str_enum(self.download_type, _enums.DownloadType)
 
     @property
     def id(self) -> int:
         return self.asset_id
+
+    @property
+    def asset_type_enum(self) -> "_enums.AssetType":
+        return self._asset_type_enum
+
+    @property
+    def download_type_enum(self) -> "_enums.DownloadType":
+        return self._download_type_enum
