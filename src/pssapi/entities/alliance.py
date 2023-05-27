8400ce51dfa214bdf49dcdc0b5b336e5f55aaec6
The worst <5472896+Zukunftsmusik@users.noreply.github.com>
2023-05-27 03:29:48 +0200

    Added enum properties to Alliance

--- src/pssapi/entities/alliance.py
+++ src/pssapi/entities/alliance.py
@@ -1,12 +1,20 @@
-from ..types import EntityInfo as _EntityInfo
 from .entity_base import EntityWithIdBase as _EntityWithIdBase
 from .raw import AllianceRaw as _AllianceRaw
+from .. import enums as _enums
+from ..types import EntityInfo as _EntityInfo
+from ..utils import parse as _parse
 
 
 class Alliance(_AllianceRaw, _EntityWithIdBase):
     def __init__(self, alliance_info: _EntityInfo) -> None:
         super().__init__(alliance_info)
+        self._alliance_country_code_enum: _enums.LanguageKey = _parse.pss_str_enum(self.alliance_country_code, _enums.LanguageKey)
+        _enums.Di
 
     @property
     def id(self) -> int:
         return self.alliance_id
+
+    @property
+    def alliance_country_code_enum(self) -> "_enums.LanguageKey":
+        return self._alliance_country_code_enum
