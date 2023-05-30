91b18e6ab8affd3c1724058fc92923b7894fdc7f
The worst <5472896+Zukunftsmusik@users.noreply.github.com>
2023-05-30 16:49:15 +0200

    Added enum properties to Friend

--- src/pssapi/entities/friend.py
+++ src/pssapi/entities/friend.py
@@ -1,4 +1,6 @@
+from .. import enums as _enums
 from ..types import EntityInfo as _EntityInfo
+from ..utils import parse as _parse
 from .entity_base import EntityWithIdBase as _EntityWithIdBase
 from .raw import FriendRaw as _FriendRaw
 
@@ -6,7 +8,12 @@ from .raw import FriendRaw as _FriendRaw
 class Friend(_FriendRaw, _EntityWithIdBase):
     def __init__(self, friend_info: _EntityInfo) -> None:
         super().__init__(friend_info)
+        self._friend_type_enum: _enums.FriendType = _parse.pss_str_enum(self.friend_type, _enums.FriendType)
 
     @property
     def id(self) -> int:
         return self.id_
+
+    @property
+    def friend_type_enum(self) -> "_enums.FriendType":
+        return self._friend_type_enum
