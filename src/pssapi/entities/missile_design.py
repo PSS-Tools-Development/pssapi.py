be55799cb6994e6b6be593b40c0c0fc29c500290
The worst <5472896+Zukunftsmusik@users.noreply.github.com>
2023-06-01 13:47:21 +0200

    Added enum properties to MissileDesign

--- src/pssapi/entities/missile_design.py
+++ src/pssapi/entities/missile_design.py
@@ -1,4 +1,6 @@
+from .. import enums as _enums
 from ..types import EntityInfo as _EntityInfo
+from ..utils import parse as _parse
 from .entity_base import EntityWithIdBase as _EntityWithIdBase
 from .raw import MissileDesignRaw as _MissileDesignRaw
 
@@ -6,7 +8,22 @@ from .raw import MissileDesignRaw as _MissileDesignRaw
 class MissileDesign(_MissileDesignRaw, _EntityWithIdBase):
     def __init__(self, missile_design_info: _EntityInfo) -> None:
         super().__init__(missile_design_info)
+        self._explosion_type_enum: _enums.ExplosionType = _parse.pss_str_enum(self.explosion_type, _enums.ExplosionType)
+        self._flight_type_enum: _enums.FlightType = _parse.pss_str_enum(self.flight_type, _enums.FlightType)
+        self._missile_type_enum: _enums.MissileType = _parse.pss_str_enum(self.missile_type, _enums.MissileType)
 
     @property
     def id(self) -> int:
         return self.missile_design_id
+
+    @property
+    def explosion_type_enum(self) -> "_enums.ExplosionType":
+        return self._explosion_type_enum
+
+    @property
+    def flight_type_enum(self) -> "_enums.FlightType":
+        return self._flight_type_enum
+
+    @property
+    def missile_type_enum(self) -> "_enums.MissileType":
+        return self._missile_type_enum
