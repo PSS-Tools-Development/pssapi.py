c7fc860c870d015e88163f2a16aa0690cdf68728
The worst <5472896+Zukunftsmusik@users.noreply.github.com>
2023-06-01 16:14:48 +0200

    Add enum properties to PromotionDesign

--- src/pssapi/entities/promotion_design.py
+++ src/pssapi/entities/promotion_design.py
@@ -1,4 +1,6 @@
+from .. import enums as _enums
 from ..types import EntityInfo as _EntityInfo
+from ..utils import parse as _parse
 from .entity_base import EntityWithIdBase as _EntityWithIdBase
 from .raw import PromotionDesignRaw as _PromotionDesignRaw
 
@@ -6,7 +8,22 @@ from .raw import PromotionDesignRaw as _PromotionDesignRaw
 class PromotionDesign(_PromotionDesignRaw, _EntityWithIdBase):
     def __init__(self, promotion_design_info: _EntityInfo) -> None:
         super().__init__(promotion_design_info)
+        self._flags_enum: _enums.PromotionDesignFlag = _parse.pss_int_flag(self.flags, _enums.PromotionDesignFlag)
+        self._promotion_type_enum: _enums.PromotionType = _parse.pss_str_enum(self.promotion_type, _enums.PromotionType)
+        self._purchase_mask_enum: _enums.SaleItemMask = _parse.pss_int_flag(self.purchase_mask, _enums.SaleItemMask)
 
     @property
-    def id(self) -> int:
+    def flags_enum(self) -> int:
+        return self._flags_enum
+
+    @property
+    def id(self) -> "_enums.PromotionDesignFlag":
         return self.promotion_design_id
+
+    @property
+    def promotion_type_enum(self) -> "_enums.PromotionType":
+        return self._promotion_type_enum
+
+    @property
+    def purchase_mask_enum(self) -> "_enums.SaleItemMask":
+        return self._purchase_mask_enum
