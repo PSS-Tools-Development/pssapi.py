69ad1bc415d2ed96c60a39f3dc21c82f0949bfd5
The worst <5472896+Zukunftsmusik@users.noreply.github.com>
2023-06-01 17:02:10 +0200

    Add enum properties to RewardDesign

--- src/pssapi/entities/reward_design.py
+++ src/pssapi/entities/reward_design.py
@@ -1,4 +1,6 @@
+from .. import enums as _enums
 from ..types import EntityInfo as _EntityInfo
+from ..utils import parse as _parse
 from .entity_base import EntityWithIdBase as _EntityWithIdBase
 from .raw import RewardDesignRaw as _RewardDesignRaw
 
@@ -6,7 +8,20 @@ from .raw import RewardDesignRaw as _RewardDesignRaw
 class RewardDesign(_RewardDesignRaw, _EntityWithIdBase):
     def __init__(self, reward_design_info: _EntityInfo) -> None:
         super().__init__(reward_design_info)
+        self._flags_enum: _enums.UserStatus = _parse.pss_int_flag(self.flags, _enums.UserStatus)
+        self._reward_type_enum: _enums.RewardType = _parse.pss_str_enum(self.reward_type, _enums.RewardType)
 
     @property
     def id(self) -> int:
         return self.reward_design_id
+
+    @property
+    def flags_enum(self) -> "_enums.UserStatus":
+        """
+        Only UserStatus.PURCHASED_TOURNAMENT_BOX... values are usually used.
+        """
+        return self._flags_enum
+
+    @property
+    def reward_type_enum(self) -> "_enums.RewardType":
+        return self._reward_type_enum
