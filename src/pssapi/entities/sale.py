640595e1d344939c116f859240896687e78fb657
The worst <5472896+Zukunftsmusik@users.noreply.github.com>
2023-06-01 18:34:44 +0200

    Add enum properties to Sale

--- src/pssapi/entities/sale.py
+++ src/pssapi/entities/sale.py
@@ -1,4 +1,6 @@
+from .. import enums as _enums
 from ..types import EntityInfo as _EntityInfo
+from ..utils import parse as _parse
 from .entity_base import EntityWithIdBase as _EntityWithIdBase
 from .raw import SaleRaw as _SaleRaw
 
@@ -6,7 +8,17 @@ from .raw import SaleRaw as _SaleRaw
 class Sale(_SaleRaw, _EntityWithIdBase):
     def __init__(self, sale_info: _EntityInfo) -> None:
         super().__init__(sale_info)
+        self._currency_type_enum: _enums.CurrencyType = _parse.pss_str_enum(self.currency_type, _enums.CurrencyType)
+        self._sale_status_enum: _enums.SaleStatus = _parse.pss_str_enum(self.sale_status, _enums.SaleStatus)
 
     @property
     def id(self) -> int:
         return self.sale_id
+
+    @property
+    def currency_type_enum(self) -> "_enums.CurrencyType":
+        return self._currency_type_enum
+
+    @property
+    def sale_status_enum(self) -> "_enums.SaleStatus":
+        return self._sale_status_enum
