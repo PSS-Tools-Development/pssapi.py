f8cd110776e1b97bb11ada1406466c3cfe28fef4
The worst <5472896+Zukunftsmusik@users.noreply.github.com>
2023-06-02 10:33:38 +0200

    Add enum properties to UserSeason

--- src/pssapi/entities/user_season.py
+++ src/pssapi/entities/user_season.py
@@ -1,4 +1,6 @@
+from .. import enums as _enums
 from ..types import EntityInfo as _EntityInfo
+from ..utils import parse as _parse
 from .entity_base import EntityWithIdBase as _EntityWithIdBase
 from .raw import UserSeasonRaw as _UserSeasonRaw
 
@@ -6,7 +8,12 @@ from .raw import UserSeasonRaw as _UserSeasonRaw
 class UserSeason(_UserSeasonRaw, _EntityWithIdBase):
     def __init__(self, user_season_info: _EntityInfo) -> None:
         super().__init__(user_season_info)
+        self._purchase_vip_status_enum: _enums.PurchaseVIPStatus = _parse.pss_str_enum(self.purchase_vip_status, _enums.PurchaseVIPStatus)
 
     @property
     def id(self) -> int:
         return self.user_season_id
+
+    @property
+    def purchase_vip_status_enum(self) -> "_enums.PurchaseVIPStatus":
+        return self._purchase_vip_status_enum
