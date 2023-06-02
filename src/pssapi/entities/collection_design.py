a0e5c48d03d81e323d39ad56e5a6067385137b59
The worst <5472896+Zukunftsmusik@users.noreply.github.com>
2023-06-02 14:01:18 +0200

    Update comment

--- src/pssapi/entities/collection_design.py
+++ src/pssapi/entities/collection_design.py
@@ -10,7 +10,7 @@ class CollectionDesign(_CollectionDesignRaw, _EntityWithIdBase):
         super().__init__(collection_design_info)
         self._collection_type_enum: _enums.CollectionType = _parse.pss_str_enum(self.collection_type, _enums.CollectionType)
         self._enhancement_type_enum: _enums.EnhancementType = _parse.pss_str_enum(self.enhancement_type, _enums.EnhancementType)
-        # self._flags_enum: _enums. = _parse.pss_str_enum(self.flags, _enums.) # Unknown enum
+        # self._flags_enum: _enums. = _parse.pss_str_enum(self.flags, _enums.)   # This enum does not exist up to PSS v0.994.1
 
     @property
     def id(self) -> int:
