8a5f809bb2b7014a59483795e2df8c0f4d654c22
The worst <5472896+Zukunftsmusik@users.noreply.github.com>
2023-06-04 18:33:27 +0200

    Add UserFlagsObject

--- src/pssapi/enums/user_flags.py
+++ src/pssapi/enums/user_flags.py
@@ -1,11 +1,21 @@
 from enum import IntFlag as _IntFlag
 
-"""
-This file has been be generated from decompilation and might require manual
-fixing, if Savy uses enum values that are python keywords.
-"""
+from .int_flag_object_base import IntFlagObjectBase as _IntFlagObjectBase
 
 
 class UserFlags(_IntFlag):
     NONE = 0
     ALLOW_FORCE_B_TEST = 1
+
+
+class UserFlagsObject(_IntFlagObjectBase):
+    def __init__(self, user_flags: UserFlags):
+        super().__init__(user_flags)
+
+    @property
+    def allow_force_b_test(self) -> bool:
+        return bool(self.value & UserFlags.ALLOW_FORCE_B_TEST)
+
+    @property
+    def value(self) -> UserFlags:
+        return self._value
