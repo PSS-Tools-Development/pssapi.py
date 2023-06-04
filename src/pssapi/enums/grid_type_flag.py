985b604c8ae250a66da181d8947a5dcdc7702fc3
The worst <5472896+Zukunftsmusik@users.noreply.github.com>
2023-06-04 16:52:47 +0200

    Add GridTypeFlagObject

--- src/pssapi/enums/grid_type_flag.py
+++ src/pssapi/enums/grid_type_flag.py
@@ -1,7 +1,26 @@
 from enum import IntFlag as _IntFlag
 
+from .int_flag_object_base import IntFlagObjectBase as _IntFlagObjectBase
+
 
 class GridTypeFlag(_IntFlag):
     NONE = 0
     A = 1
     B = 2
+
+
+class GridTypeFlagObject(_IntFlagObjectBase):
+    def __init__(self, grid_type_flag: GridTypeFlag):
+        super().__init__(grid_type_flag)
+
+    @property
+    def a(self) -> bool:
+        return bool(self.value & GridTypeFlag.A)
+
+    @property
+    def b(self) -> bool:
+        return bool(self.value & GridTypeFlag.B)
+
+    @property
+    def value(self) -> GridTypeFlag:
+        return self._value
