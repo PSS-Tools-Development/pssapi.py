88d87c9f94c7f5408617c080b2fe7081be546c9d
The worst <5472896+Zukunftsmusik@users.noreply.github.com>
2023-06-04 17:36:01 +0200

    Add MissionEventFlagObject

--- src/pssapi/enums/mission_event_flag.py
+++ src/pssapi/enums/mission_event_flag.py
@@ -1,5 +1,7 @@
 from enum import IntFlag as _IntFlag
 
+from .int_flag_object_base import IntFlagObjectBase as _IntFlagObjectBase
+
 """
 This file has been be generated from decompilation and might require manual
 fixing, if Savy uses enum values that are python keywords.
@@ -9,3 +11,16 @@ fixing, if Savy uses enum values that are python keywords.
 class MissionEventFlag(_IntFlag):
     NONE = 0
     SHOW_OPTION_IF_REQUIREMENT_NOT_MET = 1
+
+
+class MissionEventFlagObject(_IntFlagObjectBase):
+    def __init__(self, mission_event_flag: MissionEventFlag):
+        super().__init__(mission_event_flag)
+
+    @property
+    def show_option_if_requirement_not_met(self) -> bool:
+        return bool(self.value & MissionEventFlag.SHOW_OPTION_IF_REQUIREMENT_NOT_MET)
+
+    @property
+    def value(self) -> MissionEventFlag:
+        return self._value
