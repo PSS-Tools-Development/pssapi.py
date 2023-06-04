7d3fe94ca0d57937ce9c1446e90d4aa4a9435be9
The worst <5472896+Zukunftsmusik@users.noreply.github.com>
2023-06-04 16:45:05 +0200

    Add FeatureFlagObject

--- src/pssapi/enums/feature_flag.py
+++ src/pssapi/enums/feature_flag.py
@@ -1,5 +1,7 @@
 from enum import IntFlag as _IntFlag
 
+from .int_flag_object_base import IntFlagObjectBase as _IntFlagObjectBase
+
 
 class FeatureFlag(_IntFlag):
     NONE = 0
@@ -8,3 +10,32 @@ class FeatureFlag(_IntFlag):
     SHOP_TOPBAR = 4
     APPSFLYER = 8
     BRANCH = 16
+
+
+class FeatureFlagObject(_IntFlagObjectBase):
+    def __init__(self, equipment_mask_flag: FeatureFlag):
+        super().__init__(equipment_mask_flag)
+
+    @property
+    def appsflyer(self) -> bool:
+        return bool(self.value & FeatureFlag.APPSFLYER)
+
+    @property
+    def branch(self) -> bool:
+        return bool(self.value & FeatureFlag.BRANCH)
+
+    @property
+    def galaxy(self) -> bool:
+        return bool(self.value & FeatureFlag.GALAXY)
+
+    @property
+    def room_rearm(self) -> bool:
+        return bool(self.value & FeatureFlag.ROOM_REARM)
+
+    @property
+    def shop_topbar(self) -> bool:
+        return bool(self.value & FeatureFlag.SHOP_TOPBAR)
+
+    @property
+    def value(self) -> FeatureFlag:
+        return self._value
