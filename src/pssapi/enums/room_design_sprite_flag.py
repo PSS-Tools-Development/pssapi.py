2c07d705db4542bd6a42a1a98b91702575b054ca
The worst <5472896+Zukunftsmusik@users.noreply.github.com>
2023-06-04 17:39:01 +0200

    Add RoomDesignSpriteFlagObject

--- src/pssapi/enums/room_design_sprite_flag.py
+++ src/pssapi/enums/room_design_sprite_flag.py
@@ -1,11 +1,21 @@
 from enum import IntFlag as _IntFlag
 
-"""
-This file has been be generated from decompilation and might require manual
-fixing, if Savy uses enum values that are python keywords.
-"""
+from .int_flag_object_base import IntFlagObjectBase as _IntFlagObjectBase
 
 
 class RoomDesignSpriteFlag(_IntFlag):
     NONE = 0
     HIDE_IF_REQUIREMENT_NOT_MET = 1
+
+
+class RoomDesignSpriteFlagObject(_IntFlagObjectBase):
+    def __init__(self, room_design_sprite_flag: RoomDesignSpriteFlag):
+        super().__init__(room_design_sprite_flag)
+
+    @property
+    def hide_if_requirement_not_met(self) -> bool:
+        return bool(self.value & RoomDesignSpriteFlag.HIDE_IF_REQUIREMENT_NOT_MET)
+
+    @property
+    def value(self) -> RoomDesignSpriteFlag:
+        return self._value
