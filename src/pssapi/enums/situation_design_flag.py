e8ec2fad6cf6fb9740d4564305e362dd908e91c6
The worst <5472896+Zukunftsmusik@users.noreply.github.com>
2023-06-04 18:31:46 +0200

    Add SituationDesignFlagObject

--- src/pssapi/enums/situation_design_flag.py
+++ src/pssapi/enums/situation_design_flag.py
@@ -1,16 +1,46 @@
 from enum import IntFlag as _IntFlag
 
-"""
-This file has been be generated from decompilation and might require manual
-fixing, if Savy uses enum values that are python keywords.
-"""
+from .int_flag_object_base import IntFlagObjectBase as _IntFlagObjectBase
 
 
 class SituationDesignFlag(_IntFlag):
     NONE = 0
     AFFECT_ATTACKING_SHIP = 1
     AFFECT_DEFENDING_SHIP = 2
-    AFFECT_PV_P = 4
-    AFFECT_PV_E = 8
+    AFFECT_PVP = 4
+    AFFECT_PVE = 8
     VOTES = 16
     AFFECT_TOURNAMENT = 32
+
+
+class SituationDesignFlagObject(_IntFlagObjectBase):
+    def __init__(self, situation_design_flag: SituationDesignFlag):
+        super().__init__(situation_design_flag)
+
+    @property
+    def affect_attacking_ship(self) -> bool:
+        return bool(self.value & SituationDesignFlag.AFFECT_ATTACKING_SHIP)
+
+    @property
+    def affect_defending_ship(self) -> bool:
+        return bool(self.value & SituationDesignFlag.AFFECT_DEFENDING_SHIP)
+
+    @property
+    def affect_pve(self) -> bool:
+        return bool(self.value & SituationDesignFlag.AFFECT_PVE)
+
+    @property
+    def affect_pvp(self) -> bool:
+        return bool(self.value & SituationDesignFlag.AFFECT_PVP)
+
+    @property
+    def affect_tournament(self) -> bool:
+        return bool(self.value & SituationDesignFlag.AFFECT_TOURNAMENT)
+
+    @property
+    def votes(self) -> bool:
+        return bool(self.value & SituationDesignFlag.VOTES)
+
+    @property
+    def value(self) -> SituationDesignFlag:
+        return self._value
