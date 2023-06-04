d5ef933571361c011135063790b97e4064ea9a85
The worst <5472896+Zukunftsmusik@users.noreply.github.com>
2023-06-04 16:39:34 +0200

    Add ConditionTypeAvailabilityMaskObject

--- src/pssapi/enums/condition_type_availability_mask.py
+++ src/pssapi/enums/condition_type_availability_mask.py
@@ -1,7 +1,26 @@
 from enum import IntFlag as _IntFlag
 
+from .int_flag_object_base import IntFlagObjectBase as _IntFlagObjectBase
+
 
 class ConditionTypeAvailabilityMask(_IntFlag):
     NONE = 0
     TARGET_ROOM = 1
     TARGET_CHARACTER = 2
+
+
+class ConditionTypeAvailabilityMaskObject(_IntFlagObjectBase):
+    def __init__(self, character_flags: ConditionTypeAvailabilityMask):
+        super().__init__(character_flags)
+
+    @property
+    def target_character(self) -> bool:
+        return bool(self.value & ConditionTypeAvailabilityMask.TARGET_CHARACTER)
+
+    @property
+    def target_room(self) -> bool:
+        return bool(self.value & ConditionTypeAvailabilityMask.TARGET_ROOM)
+
+    @property
+    def value(self) -> ConditionTypeAvailabilityMask:
+        return self._value
