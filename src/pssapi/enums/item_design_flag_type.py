cb4fbbff37eef00f5edef5ad9325f49f9887fe83
The worst <5472896+Zukunftsmusik@users.noreply.github.com>
2023-06-04 17:10:28 +0200

    Add ItemDesignFlagTypeObject

--- src/pssapi/enums/item_design_flag_type.py
+++ src/pssapi/enums/item_design_flag_type.py
@@ -1,5 +1,7 @@
 from enum import IntFlag as _IntFlag
 
+from .int_flag_object_base import IntFlagObjectBase as _IntFlagObjectBase
+
 """
 This file has been be generated from decompilation and might require manual
 fixing, if Savy uses enum values that are python keywords.
@@ -26,3 +28,84 @@ class ItemDesignFlagType(_IntFlag):
     UNIQUE_CONTENT_REWARD = 32768
     USE_ON_ROOM = 65536
     DONT_SHOW_REWARDS = 131072
+
+
+class ItemDesignFlagTypeObject(_IntFlagObjectBase):
+    def __init__(self, item_design_flag_type: ItemDesignFlagType):
+        super().__init__(item_design_flag_type)
+
+    @property
+    def align_to_top(self) -> bool:
+        return bool(self.value & ItemDesignFlagType.ALIGN_TO_TOP)
+
+    @property
+    def allow_donation(self) -> bool:
+        return bool(self.value & ItemDesignFlagType.ALLOW_DONATION)
+
+    @property
+    def allow_manufacture(self) -> bool:
+        return bool(self.value & ItemDesignFlagType.ALLOW_MANUFACTURE)
+
+    @property
+    def allow_trading(self) -> bool:
+        return bool(self.value & ItemDesignFlagType.ALLOW_TRADING)
+
+    @property
+    def challenge_reward(self) -> bool:
+        return bool(self.value & ItemDesignFlagType.CHALLENGE_REWARD)
+
+    @property
+    def consumable(self) -> bool:
+        return bool(self.value & ItemDesignFlagType.CONSUMABLE)
+
+    @property
+    def dont_show_rewards(self) -> bool:
+        return bool(self.value & ItemDesignFlagType.DONT_SHOW_REWARDS)
+
+    @property
+    def dropship_reward(self) -> bool:
+        return bool(self.value & ItemDesignFlagType.DROPSHIP_REWARD)
+
+    @property
+    def giftable(self) -> bool:
+        return bool(self.value & ItemDesignFlagType.GIFTABLE)
+
+    @property
+    def instant_reward(self) -> bool:
+        return bool(self.value & ItemDesignFlagType.INSTANT_REWARD)
+
+    @property
+    def mission_reward(self) -> bool:
+        return bool(self.value & ItemDesignFlagType.MISSION_REWARD)
+
+    @property
+    def pvp_reward(self) -> bool:
+        return bool(self.value & ItemDesignFlagType.PVP_REWARD)
+
+    @property
+    def recycling_build(self) -> bool:
+        return bool(self.value & ItemDesignFlagType.RECYCLING_BUILD)
+
+    @property
+    def use_on_crew(self) -> bool:
+        return bool(self.value & ItemDesignFlagType.USE_ON_CREW)
+
+    @property
+    def used_item(self) -> bool:
+        return bool(self.value & ItemDesignFlagType.USED_ITEM)
+
+    @property
+    def task_prize(self) -> bool:
+        return bool(self.value & ItemDesignFlagType.TASK_PRIZE)
+
+    @property
+    def unique_content_reward(self) -> bool:
+        return bool(self.value & ItemDesignFlagType.UNIQUE_CONTENT_REWARD)
+
+    @property
+    def use_on_room(self) -> bool:
+        return bool(self.value & ItemDesignFlagType.USE_ON_ROOM)
+
+    @property
+    def value(self) -> ItemDesignFlagType:
+        return self._value
