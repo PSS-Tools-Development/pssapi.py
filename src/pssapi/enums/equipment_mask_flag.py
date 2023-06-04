332e0130e629fcbc04a5cc33bf3992dbe3f6171a
The worst <5472896+Zukunftsmusik@users.noreply.github.com>
2023-06-04 16:43:41 +0200

    Add EquipmentMaskFlagObject

--- src/pssapi/enums/equipment_mask_flag.py
+++ src/pssapi/enums/equipment_mask_flag.py
@@ -1,5 +1,7 @@
 from enum import IntFlag as _IntFlag
 
+from .int_flag_object_base import IntFlagObjectBase as _IntFlagObjectBase
+
 
 class EquipmentMaskFlag(_IntFlag):
     NONE = 0
@@ -9,3 +11,36 @@ class EquipmentMaskFlag(_IntFlag):
     WEAPON = 8
     ACCESSORY = 16
     PET = 32
+
+
+class EquipmentMaskFlagObject(_IntFlagObjectBase):
+    def __init__(self, equipment_mask_flag: EquipmentMaskFlag):
+        super().__init__(equipment_mask_flag)
+
+    @property
+    def accessory(self) -> bool:
+        return bool(self.value & EquipmentMaskFlag.ACCESSORY)
+
+    @property
+    def body(self) -> bool:
+        return bool(self.value & EquipmentMaskFlag.BODY)
+
+    @property
+    def head(self) -> bool:
+        return bool(self.value & EquipmentMaskFlag.HEAD)
+
+    @property
+    def leg(self) -> bool:
+        return bool(self.value & EquipmentMaskFlag.LEG)
+
+    @property
+    def pet(self) -> bool:
+        return bool(self.value & EquipmentMaskFlag.PET)
+
+    @property
+    def weapon(self) -> bool:
+        return bool(self.value & EquipmentMaskFlag.WEAPON)
+
+    @property
+    def value(self) -> EquipmentMaskFlag:
+        return self._value
