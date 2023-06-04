15e5001b4621004b16845a4f6b919da1538e04e0
The worst <5472896+Zukunftsmusik@users.noreply.github.com>
2023-06-04 16:04:21 +0200

    Add comment

--- src/pssapi/enums/int_flag_object_base.py
+++ src/pssapi/enums/int_flag_object_base.py
@@ -7,4 +7,5 @@ class IntFlagObjectBase(object):
 
     @property
     def value(self) -> _IntFlag:
+        """Override this property in inheriting classes with the correct data type for code completion to work."""
         return self._value
