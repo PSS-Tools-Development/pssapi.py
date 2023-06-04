57ed533b7d8b902ac702db8059fe5379989df9b1
The worst <5472896+Zukunftsmusik@users.noreply.github.com>
2023-06-04 16:36:34 +0200

    Add CharacterFlagsObject

--- src/pssapi/enums/character_flags.py
+++ src/pssapi/enums/character_flags.py
@@ -1,5 +1,7 @@
 from enum import IntFlag as _IntFlag
 
+from .int_flag_object_base import IntFlagObjectBase as _IntFlagObjectBase
+
 """
 This file has been be generated from decompilation and might require manual
 fixing, if Savy uses enum values that are python keywords.
@@ -9,3 +11,16 @@ fixing, if Savy uses enum values that are python keywords.
 class CharacterFlags(_IntFlag):
     NONE = 0
     FAVOURITE = 1
+
+
+class CharacterFlagsObject(_IntFlagObjectBase):
+    def __init__(self, character_flags: CharacterFlags):
+        super().__init__(character_flags)
+
+    @property
+    def favourite(self) -> bool:
+        return self.value & CharacterFlags.FAVOURITE or False
+
+    @property
+    def value(self) -> CharacterFlags:
+        return self._value
