c84a8161ddea6b461d6def20f35d8c4930461fec
The worst <5472896+Zukunftsmusik@users.noreply.github.com>
2023-06-04 16:34:02 +0200

    Add BehaviorFlagsObject

--- src/pssapi/enums/behavior_flags.py
+++ src/pssapi/enums/behavior_flags.py
@@ -1,5 +1,7 @@
 from enum import IntFlag as _IntFlag
 
+from .int_flag_object_base import IntFlagObjectBase as _IntFlagObjectBase
+
 """
 This file has been be generated from decompilation and might require manual
 fixing, if Savy uses enum values that are python keywords.
@@ -8,3 +10,12 @@ fixing, if Savy uses enum values that are python keywords.
 
 class BehaviorFlags(_IntFlag):
     NONE = 0
+
+
+class BehaviorFlagsObject(_IntFlagObjectBase):
+    def __init__(self, behavior_flags: BehaviorFlags):
+        super().__init__(behavior_flags)
+
+    @property
+    def value(self) -> BehaviorFlags:
+        return self._value
