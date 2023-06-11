1305f47080b754093bc80e7f2a521941eb0ca6e8
The worst <5472896+Zukunftsmusik@users.noreply.github.com>
2023-06-11 14:03:11 +0200

    Add _PromotionDesignMetadata

--- src/pssapi/entities/metadata/__init__.py
+++ src/pssapi/entities/metadata/__init__.py
@@ -1,9 +1,11 @@
 from .item_design_metadata import ItemDesignMetadata
 from .missile_design_metadata import MissileDesignMetadata
 from .mission_design_metadata import MissionDesignMetadata
+from .promotion_design_metadata import PromotionDesignMetadata
 
 all = [
     ItemDesignMetadata.__name__,
     MissileDesignMetadata.__name__,
     MissionDesignMetadata.__name__,
+    PromotionDesignMetadata.__name__,
 ]
