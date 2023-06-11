bacdfde0dc592e04457622490007ea5a89e01afb
The worst <5472896+Zukunftsmusik@users.noreply.github.com>
2023-06-11 14:26:06 +0200

    Add RewardDesignMetadata

--- src/pssapi/entities/metadata/__init__.py
+++ src/pssapi/entities/metadata/__init__.py
@@ -2,10 +2,12 @@ from .item_design_metadata import ItemDesignMetadata
 from .missile_design_metadata import MissileDesignMetadata
 from .mission_design_metadata import MissionDesignMetadata
 from .promotion_design_metadata import PromotionDesignMetadata
+from .reward_design_metadata import RewardDesignMetadata
 
 all = [
     ItemDesignMetadata.__name__,
     MissileDesignMetadata.__name__,
     MissionDesignMetadata.__name__,
     PromotionDesignMetadata.__name__,
+    RewardDesignMetadata.__name__,
 ]
