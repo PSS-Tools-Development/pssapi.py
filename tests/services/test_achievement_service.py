b820c8ca517ba64ebe0fab85e6f42a2830b86ead
The worst <5472896+Zukunftsmusik@users.noreply.github.com>
2023-06-18 11:46:19 +0200

    Add vcr cassette

--- tests/services/test_achievement_service.py
+++ tests/services/test_achievement_service.py
@@ -1,8 +1,10 @@
 import pytest
+import vcr
 
 from pssapi import PssApiClient
 
 
+@vcr.use_cassette("tests/cassettes/list_achievement_designs.yaml")
 @pytest.mark.asyncio
 @pytest.mark.usefixtures("client")
 async def test_list_achievement_designs(client: PssApiClient):
