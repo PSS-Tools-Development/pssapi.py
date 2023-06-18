440c552b5cf3c6db13b53178d3b9c011f745c9de
The worst <5472896+Zukunftsmusik@users.noreply.github.com>
2023-06-18 11:41:39 +0200

    Add fixture for client

---
+++ tests/services/conftest.py
@@ -0,0 +1,9 @@
+import pytest
+from pssapi import PssApiClient
+from pssapi import enums
+
+
+@pytest.fixture(scope='session')  # one server to rule'em all
+def client() -> PssApiClient:
+    client = PssApiClient(device_type=enums.DeviceType.ANDROID, language_key=enums.LanguageKey.ENGLISH, production_server='api.pixelstarships.com')
+    return client
