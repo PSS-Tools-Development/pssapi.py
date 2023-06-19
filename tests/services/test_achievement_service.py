ebc72c3177f0f0487f465831db647730f63dd4ac
The worst <5472896+Zukunftsmusik@users.noreply.github.com>
2023-06-20 00:14:15 +0200

    Add some meaningful tests

--- tests/services/test_achievement_service.py
+++ tests/services/test_achievement_service.py
@@ -1,10 +1,13 @@
 import pytest
 
-from pssapi import PssApiClient
+import pssapi
 
 
 @pytest.mark.asyncio
 @pytest.mark.usefixtures("client")
 @pytest.mark.vcr()
-async def test_list_achievement_designs(client: PssApiClient):
-    await client.achievement_service.list_achievement_designs()
+async def test_list_achievement_designs(client: pssapi.PssApiClient):
+    achievement_designs = await client.achievement_service.list_achievement_designs()
+    assert isinstance(achievement_designs, list)
+    assert len(achievement_designs) > 0
+    assert isinstance(achievement_designs[0], pssapi.entities.AchievementDesign)
--- tests/services/test_collection_service.py
+++ tests/services/test_collection_service.py
@@ -1,10 +1,13 @@
 import pytest
 
-from pssapi import PssApiClient
+import pssapi
 
 
 @pytest.mark.asyncio
 @pytest.mark.usefixtures("client")
 @pytest.mark.vcr()
-async def test_list_all_collection_designs(client: PssApiClient):
-    await client.collection_service.list_all_collection_designs()
+async def test_list_all_collection_designs(client: pssapi.PssApiClient):
+    collection_designs = await client.collection_service.list_all_collection_designs()
+    assert isinstance(collection_designs, list)
+    assert len(collection_designs) > 0
+    assert isinstance(collection_designs[0], pssapi.entities.CollectionDesign)
