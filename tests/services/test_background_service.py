c3949b654bc9cbbc58322ce16731a8d364f2870f
The worst <5472896+Zukunftsmusik@users.noreply.github.com>
2023-06-19 23:58:09 +0200

    Fix test function name

--- tests/services/test_background_service.py
+++ tests/services/test_background_service.py
@@ -6,5 +6,5 @@ from pssapi import PssApiClient
 @pytest.mark.asyncio
 @pytest.mark.usefixtures("client")
 @pytest.mark.vcr()
-async def test_list_animations(client: PssApiClient):
+async def test_list_backgrounds(client: PssApiClient):
     await client.background_service.list_backgrounds()
