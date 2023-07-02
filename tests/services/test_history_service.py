0c4a3b73f38adfef9cae824e8f31fe5d21994af9
The worst <5472896+Zukunftsmusik@users.noreply.github.com>
2023-07-02 18:06:56 +0200

    Fix test function name

--- tests/services/test_history_service.py
+++ tests/services/test_history_service.py
@@ -8,6 +8,6 @@ ITEM_DESIGN_ID: int = 778  # Void Particle
 @pytest.mark.asyncio
 @pytest.mark.usefixtures("client")
 @pytest.mark.vcr()
-async def test_go_to(client: pssapi.PssApiClient):
+async def test_price_history(client: pssapi.PssApiClient):
     history = await client.history_service.price_history(ITEM_DESIGN_ID)
     assert isinstance(history, pssapi.entities.History)
