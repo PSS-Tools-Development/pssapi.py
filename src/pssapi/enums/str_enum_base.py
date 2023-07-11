2db4e21ee403dfd27100d55f59798a2c730e2fc5
The worst <5472896+Zukunftsmusik@users.noreply.github.com>
2023-07-11 17:24:07 +0200

    Fix StrEnumBase to return NONE member on wrong member data type

--- src/pssapi/enums/str_enum_base.py
+++ src/pssapi/enums/str_enum_base.py
@@ -4,8 +4,24 @@ from enum import StrEnum as _StrEnum
 class StrEnumBase(_StrEnum):
     @classmethod
     def _missing_(cls, value: str):
-        value_lower = value.lower()
-        for member in cls:
-            if member.lower() == value_lower:
-                return member
-        raise ValueError(f"'{value}' is not a valid {cls.__name__}")
+        if isinstance(value, str):
+            try:
+                float_value = float(value)
+                if not float_value:
+                    return cls("None")
+            except:
+                pass
+
+            try:
+                int_value = int(value)
+                if not int_value:
+                    return cls("None")
+            except:
+                pass
+
+            value_lower = value.lower()
+            for member in cls:
+                if member.lower() == value_lower:
+                    return member
+            raise ValueError(f"'{value}' is not a valid {cls.__qualname__}")
+        return cls("None")
