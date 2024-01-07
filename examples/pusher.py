7cab3c875e0d759138e097d07ecf017e3c0bd46b
architelos <technopiabcde@gmail.com>
2024-01-07 15:59:47 +0700

    Ignore test files, add examples

--- examples/pusher.py
+++ examples/pusher.py
@@ -1,7 +1,8 @@
-from asyncio import new_event_loop, set_event_loop
+from asyncio import get_event_loop
 
+from pssapi.pusher import Pusher
+from pssapi.pusher import Channel
 from pssapi.enums import PusherChannelType
-from pssapi.pusher import Channel, Pusher
 
 
 async def pusher_example():
