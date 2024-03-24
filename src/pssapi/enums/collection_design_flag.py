b8cde055f5009c5ba5871c5150016f4a6ab787f1
The worst <5472896+Zukunftsmusik@users.noreply.github.com>
2024-03-24 12:24:55 +0100

    Update CollectionDesign

---
+++ src/pssapi/enums/collection_design_flag.py
@@ -0,0 +1,11 @@
+from enum import IntFlag as _IntFlag
+
+"""
+This file has been be generated from decompilation and might require manual
+fixing, if Savy uses enum values that are python keywords.
+"""
+
+
+class CollectionDesignFlag(_IntFlag):
+    NONE = 0
+    HIDDEN = 1
