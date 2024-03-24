975db9f1253c61bafa2edcf604c05bb0058dbc43
The worst <5472896+Zukunftsmusik@users.noreply.github.com>
2024-03-24 18:16:48 +0100

    Fix Issue #53

--- src/pssapi/entities/alliance.py
+++ src/pssapi/entities/alliance.py
@@ -8,12 +8,7 @@ from .raw import AllianceRaw as _AllianceRaw
 class Alliance(_AllianceRaw, _EntityWithIdBase):
     def __init__(self, alliance_info: _EntityInfo) -> None:
         super().__init__(alliance_info)
-        self._alliance_country_code_enum: _enums.LanguageKey = _parse.pss_str_enum(self.alliance_country_code, _enums.LanguageKey)
 
     @property
     def id(self) -> int:
         return self.alliance_id
-
-    @property
-    def alliance_country_code_enum(self) -> "_enums.LanguageKey":
-        return self._alliance_country_code_enum
