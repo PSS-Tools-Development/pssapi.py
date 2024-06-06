c90ce181a32beee1467b10433976cac239efe08d
The worst <5472896+Zukunftsmusik@users.noreply.github.com>
2024-06-06 13:29:47 +0200

    Add docstring for Issue #66

--- src/pssapi/client.py
+++ src/pssapi/client.py
@@ -12,6 +12,15 @@ class PssApiClient(_client_base.PssApiClientBase):
         super()._update_services()
 
     async def device_login(self, device_key: str, checksum_key: str) -> _entities.UserLogin:
+        """Shortcut to self.user_service.device_login(), calculating the required information.
+
+        Args:
+            device_key (str): A UUID representing a "device".
+            checksum_key (str): A secret required to generate a checksum for the login.
+
+        Returns:
+            _entities.UserLogin: An object containing information on the last user logged on the device with the provided `device_key` and an access token for that user.
+        """
         client_date_time = _utils.get_utc_now()
         checksum = self.user_service.utils.create_device_login_checksum(device_key, self.device_type, client_date_time, checksum_key)
         return await self.user_service.device_login(checksum, client_date_time, device_key, self.device_type)
