14706081ab35828d7dd59bfbc7822592f873a170
The worst <5472896+Zukunftsmusik@users.noreply.github.com>
2024-06-06 13:40:01 +0200

    Release/v0.5.1 (#67)
    * Implement Issue #66
    * Implement Issue #65

--- src/pssapi/client.py
+++ src/pssapi/client.py
@@ -1,5 +1,7 @@
-import pssapi.client_base as _client_base
-import pssapi.enums as _enums
+from . import client_base as _client_base
+from . import entities as _entities
+from . import enums as _enums
+from . import utils as _utils
 
 
 class PssApiClient(_client_base.PssApiClientBase):
@@ -8,3 +10,17 @@ class PssApiClient(_client_base.PssApiClientBase):
 
     def _update_services(self):
         super()._update_services()
+
+    async def device_login(self, device_key: str, checksum_key: str) -> _entities.UserLogin:
+        """Shortcut to self.user_service.device_login(), calculating the required information.
+
+        Args:
+            device_key (str): A UUID representing a "device".
+            checksum_key (str): A secret required to generate a checksum for the login.
+
+        Returns:
+            _entities.UserLogin: An object containing information on the last user logged on the device with the provided `device_key` and an access token for that user.
+        """
+        client_date_time = _utils.get_utc_now()
+        checksum = self.user_service.utils.create_device_login_checksum(device_key, self.device_type, client_date_time, checksum_key)
+        return await self.user_service.device_login(checksum, client_date_time, device_key, self.device_type)
