7f3d6173fed778ad48cd39a991d607fbfef69b76
The worst <5472896+Zukunftsmusik@users.noreply.github.com>
2026-07-19 16:09:05 +0200

    Fix: entity Engagement enum properties

--- src/pssapi/entities/engagement.py
+++ src/pssapi/entities/engagement.py
@@ -8,8 +8,6 @@ from .raw import EngagementRaw as _EngagementRaw
 class Engagement(_EngagementRaw, _EntityWithIdBase):
     def __init__(self, engagement_info: _EntityInfo) -> None:
         super().__init__(engagement_info)
-        self._engagement_approval_state_enum: _enums.EngagementApprovalState = _parse.pss_str_enum(self.engagement_approval_state, _enums.EngagementApprovalState)
-        self._engagement_group_user_state_enum: _enums.EngagementGroupUserState = _parse.pss_str_enum(self.engagement_group_user_state, _enums.EngagementGroupUserState)
         self._engagement_type_enum: _enums.EngagementType = _parse.pss_str_enum(self.engagement_type, _enums.EngagementType)
         self._outcome_type_enum: _enums.OutcomeType = _parse.pss_str_enum(self.outcome_type, _enums.OutcomeType)
         self._scoring_type_enum: _enums.EngagementScoringType = _parse.pss_str_enum(self.scoring_type, _enums.EngagementScoringType)
@@ -17,14 +15,6 @@ class Engagement(_EngagementRaw, _EntityWithIdBase):
     @property
     def id(self) -> int:
         return self.engagement_id
-
-    @property
-    def engagement_approval_state_enum(self) -> _enums.EngagementApprovalState:
-        return self._engagement_approval_state_enum
-
-    @property
-    def engagement_group_user_state_enum(self) -> _enums.EngagementGroupUserState:
-        return self._engagement_group_user_state_enum
 
     @property
     def engagement_type_enum(self) -> _enums.EngagementType:
